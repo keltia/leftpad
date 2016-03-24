176b8bf278c5d643e3b1c2b52704fdbad0009fbb
Peter Bourgon <peter@bourgon.org>
2016-03-24 15:46:44 +0100

    Fix tests

--- leftpad_test.go
+++ leftpad_test.go
@@ -2,47 +2,47 @@
 
 /*
   These are the tests for the leftpad package.
- */
+*/
 package leftpad
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 var (
 	basic = "foo"
 )
 
-func TestLeftPad(t *testing.T) {
-	res, err := LeftPad(basic, 2)
+func TestPad(t *testing.T) {
+	res, err := Pad(basic, 2)
 	if res != basic || err != nil {
 		t.Errorf(fmt.Sprintf("Strings should match! res='%s'", res))
 	}
 
-	res, err = LeftPad(basic, 3)
+	res, err = Pad(basic, 3)
 	if res != basic || err != nil {
 		t.Errorf(fmt.Sprintf("Strings should match! res='%s'", res))
 	}
 
-	res, err = LeftPad(basic, 5)
+	res, err = Pad(basic, 5)
 	if res != "  foo" || err != nil {
 		t.Errorf(fmt.Sprintf("Error: '%s' should be '  foo'", res))
 	}
 }
 
-func TestLeftPadStr(t *testing.T) {
-	res, err := LeftPadStr(basic, 2, "X")
+func TestPadChar(t *testing.T) {
+	res, err := PadChar(basic, 2, 'X')
 	if res != basic || err != nil {
 		t.Errorf(fmt.Sprintf("Strings should match! res='%s'", res))
 	}
 
-	res, err = LeftPadStr(basic, 3, "X")
+	res, err = PadChar(basic, 3, 'X')
 	if res != basic || err != nil {
 		t.Errorf(fmt.Sprintf("Strings should match! res='%s'", res))
 	}
 
-	res, err = LeftPadStr(basic, 5, "X")
+	res, err = PadChar(basic, 5, 'X')
 	if res != "XXfoo" || err != nil {
 		t.Errorf(fmt.Sprintf("Error: '%s' should be 'XXfoo'", res))
 	}
